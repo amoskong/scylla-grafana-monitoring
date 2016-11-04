d5392873604858237c1f9606a55b275d20ee2ae8
Amos Kong <amos@scylladb.com>
2016-11-04 17:19:01 +0800

    fix directory name
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- add_datasource_metric.sh
+++ add_datasource_metric.sh
@@ -6,6 +6,6 @@ DB_IP="$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' aprom)"
 curl -XPOST -i http://localhost:3000/api/datasources \
      --data-binary '{"name":"prometheus", "type":"prometheus", "url":"'"http://$DB_IP:9090"'", "access":"proxy", "basicAuth":false}' \
      -H "Content-Type: application/json"
-curl -XPOST -i http://localhost:3000/api/dashboards/db --data-binary @./grafana/scylla-dash.json -H "Content-Type: application/json"
-curl -XPOST -i http://localhost:3000/api/dashboards/db --data-binary @./grafana/scylla-dash-per-server.json -H "Content-Type: application/json"
-curl -XPOST -i http://localhost:3000/api/dashboards/db --data-binary @./grafana/scylla-dash-io-per-server.json -H "Content-Type: application/json"
+curl -XPOST -i http://localhost:3000/api/dashboards/db --data-binary @./sct_json/scylla-dash.json -H "Content-Type: application/json"
+curl -XPOST -i http://localhost:3000/api/dashboards/db --data-binary @./sct_json/scylla-dash-per-server.json -H "Content-Type: application/json"
+curl -XPOST -i http://localhost:3000/api/dashboards/db --data-binary @./sct_json/scylla-dash-io-per-server.json -H "Content-Type: application/json"
